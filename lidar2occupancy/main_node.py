5c5080814a1022b034cf6ab464eaa6cac2b92c7f
Evgenii <Evgenii>
2023-04-26 01:12:01 +0200

    add occupancy map

--- lidar2occupancy/main_node.py
+++ lidar2occupancy/main_node.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import PointCloud2
 from rclpy.qos import qos_profile_system_default
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 from geometry_msgs.msg import TransformStamped
+from nav_msgs.msg import OccupancyGrid
 from scipy.spatial.transform import Rotation as R
 from sensor_msgs_py.point_cloud2 import read_points, create_cloud_xyz32
 import open3d as o3d
@@ -11,6 +12,7 @@ import numpy as np
 from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+import skimage
 
 
 class Lidar2Occupancy(Node):
@@ -33,11 +35,18 @@ class Lidar2Occupancy(Node):
         self._processed_pts_pub = self.create_publisher(PointCloud2, '/processed_pts', qos_profile_system_default)
         self._projected_pts_pub = self.create_publisher(PointCloud2, '/projected_pts', qos_profile_system_default)
         self._projected_pts_pub_ring = self.create_publisher(PointCloud2, '/projected_pts_ring', qos_profile_system_default)
+        self._scan_points_pub = self.create_publisher(PointCloud2, '/scan_points', qos_profile_system_default)
         self._image_pub = self.create_publisher(Image, "/image", qos_profile_system_default)
+        self._image2_pub = self.create_publisher(Image, "/image2", qos_profile_system_default)
         # image frame
         self.cv_bridge = CvBridge()
         self._image = np.zeros(np.round((4 / 0.05, 4 / 0.05)).astype(int)).astype(np.uint8)
-
+        # occupancy grid
+        self.map_publisher = self.create_publisher(
+            OccupancyGrid,
+            '/occupancy_grid',
+            qos_profile_system_default
+        )
     def listener_callback(self, msg):
         lidar_data = read_points(msg)
         np_pts = np.array([lidar_data['x'], lidar_data['y'], lidar_data['z']])
@@ -79,6 +88,26 @@ class Lidar2Occupancy(Node):
         image.reshape(-1)[pixels_num] = 255
         image.reshape((80, 80))
 
+        # transform 2d points to lidar scan
+        depths = np.linalg.norm(discretized_pts_np, axis=1)[..., None]
+        normalized_pts_np = discretized_pts_np / depths
+        scans2d = []
+        num_beams = 180
+        dr = (2 * np.pi / num_beams) / 2    # 2 * Pi == curve length
+        for alpha in np.linspace(0, 2*np.pi, num_beams):
+            curve_point = np.array([np.cos(alpha), np.sin(alpha), 0])
+            sector_pts = np.linalg.norm(normalized_pts_np - curve_point, axis=1) < dr
+            if np.any(sector_pts):
+                first_obstacle = np.argmin(depths[sector_pts])
+                scans2d.append(discretized_pts_np[sector_pts][first_obstacle])
+        lidar_scan = np.array(scans2d)
+
+        measured_cells = self.bresenham(lidar_scan)
+        # create occupancy grid
+        occupancy_grid = np.copy(self._image)
+        for free_cells in measured_cells:
+            occupancy_grid[free_cells[0], free_cells[1]] = 127
+
         # visualize all results
         # coarse_transform_msg = create_cloud_xyz32(
         #     Header(stamp=self.get_clock().now().to_msg(), frame_id=msg.header.frame_id),
@@ -107,8 +136,23 @@ class Lidar2Occupancy(Node):
 
         projected_pts_msg_ring = create_cloud_xyz32(
             Header(stamp=self.get_clock().now().to_msg(), frame_id=msg.header.frame_id),
-            discretized_pts_np / np.linalg.norm(discretized_pts_np, axis=1)[..., None]
+            discretized_pts_np / depths
         )
+        scan_points_msg = create_cloud_xyz32(
+            Header(stamp=self.get_clock().now().to_msg(), frame_id=msg.header.frame_id),
+            lidar_scan
+        )
+        # import pdb
+        # pdb.set_trace()
+        occupancy_grid_msg = OccupancyGrid()
+        occupancy_grid_msg.header.stamp = self.get_clock().now().to_msg()
+        occupancy_grid_msg.header.frame_id = msg.header.frame_id
+        occupancy_grid_msg.info.resolution = 0.05
+        occupancy_grid_msg.info.width = 80
+        occupancy_grid_msg.info.height = 80
+        occupancy_grid_msg.info.origin.position.x = -2.
+        occupancy_grid_msg.info.origin.position.y = -2.
+        occupancy_grid_msg.data = occupancy_grid.reshape(-1).tolist()
         # import pdb
         # pdb.set_trace()
         # new_msg.data = msg.data
@@ -120,10 +164,11 @@ class Lidar2Occupancy(Node):
         self._processed_pts_pub.publish(processed_pts_msg)
         self._projected_pts_pub.publish(projected_pts_msg)
         self._projected_pts_pub_ring.publish(projected_pts_msg_ring)
-        # import pdb
-        # pdb.set_trace()
-        self._image_pub.publish(self.cv_bridge.cv2_to_imgmsg(image, "mono8"))
+        self._scan_points_pub.publish(scan_points_msg)
 
+        self._image_pub.publish(self.cv_bridge.cv2_to_imgmsg(image, "mono8"))
+        self._image2_pub.publish(self.cv_bridge.cv2_to_imgmsg(occupancy_grid, "mono8"))
+        self.map_publisher.publish(occupancy_grid_msg)
     def vectors_angle(self, a, b=np.array([0, 1])):
         """ Estimates angles between 2d vectors"""
         a_norm = a / np.linalg.norm(a)
@@ -163,6 +208,15 @@ class Lidar2Occupancy(Node):
         self.transformation[2, -1] -= z_shift
         # return floor_pcd.points
 
+    def bresenham(self, scan_points):
+        pixels = np.round(
+            ((2, 2) + scan_points[:, :-1]) * (1 / 4 * 79, 1 / 4 * 79)  # normalize to image plane
+        ).astype(int)
+        coords = []
+        for end_point in pixels:
+            rr, cc = skimage.draw.line(39, 39, end_point[1], end_point[0])
+            coords.append((rr, cc))
+        return coords
 
 def main(args=None):
     rclpy.init(args=args)
